d8ec24b344dd3224874e1ebbf67796ddcca5c4a7
CWBETA-HOMEPC\titidatiti <titidatiti@cwbeta.com>
2018-05-02 17:39:29 +0800

    修复WDA启动shell脚本错误

--- src/main/resources/execWDA.sh
+++ src/main/resources/execWDA.sh
@@ -2,6 +2,6 @@
 
 set -exo pipefail
 
-PATH=$0
 UDID=$(system_profiler SPUSBDataType | sed -n -E -e '/(iPhone|iPad)/,/Serial/s/ *Serial Number: *(.+)/\1/p')
-xcodebuild -project {$PATH}/WebDriverAgent.xcodeproj -scheme WebDriverAgentRunner -destination "id=${UDID}" test
+WPA_PATH=$1
+xcodebuild -project $WPA_PATH/WebDriverAgent.xcodeproj -scheme WebDriverAgentRunner -destination "id=${UDID}" test
